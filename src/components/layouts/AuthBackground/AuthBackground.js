70c6106f34ae3661091827a4e66ce60328f9e0a8
ittipolCha <icharoenphol2001@gmail.com>
2025-05-04 23:10:18 +0700

    update remove hospital

--- src/components/layouts/AuthBackground/AuthBackground.js
+++ src/components/layouts/AuthBackground/AuthBackground.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import Logo from "assets/images/Mahidol_U.png";
+import Logo from "assets/images/Logo1.png";
 import LanguageSwitcher from "components/elements/LanguageSwitcher/LanguageSwitcher";
 import styles from "./AuthBackground.module.css";
 import { useTranslation } from "react-i18next";
@@ -16,9 +16,8 @@ const AuthBackground = ({ children, showLanguageSwitcher = false }) => {
       {screens.lg ? (
         <div className={styles.container}>
           <div className={styles.header}>
-            <img src={Logo} alt="Mahidol University" className={styles.logo} />
+            <img src={Logo} alt="Logo" className={styles.logo} />
             <p className={styles.title}>{t("background.header")}</p>
-            <p className={styles.subtitle}>{t("background.sub_header")}</p>
           </div>
           <div className={styles.body}>
             {showLanguageSwitcher ? (
--- src/components/layouts/MainLayout/MainLayout.jsx
+++ src/components/layouts/MainLayout/MainLayout.jsx
@@ -21,8 +21,8 @@ import {
   Space,
   theme,
 } from "antd";
-import Logo2 from "assets/images/Mahidol_U.png";
-import Logo from "assets/images/pattern_MU-logo.png";
+import Logo2 from "assets/images/Logo1.png";
+import Logo from "assets/images/Logo2.jpg";
 import cx from "classnames";
 import { setState } from "features/home/slices/homeSlice";
 import { useCallback, useEffect, useMemo, useState } from "react";
